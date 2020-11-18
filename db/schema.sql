b29047dd2b70baad5834d202185027e56893f22c
fintank <finapptank@gmail.com>
2020-11-18 09:57:40 -0500

    added query to node with join

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS candidates;
+
+
 CREATE TABLE parties (
   id INTEGER PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
@@ -5,10 +9,14 @@ CREATE TABLE parties (
 );
 
 
+
 CREATE TABLE candidates (
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  industry_connected BOOLEAN NOT NULL,
+  party_id INTEGER UNSIGNED,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
 
