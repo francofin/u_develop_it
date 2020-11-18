529ec179d3be43837ce4cb285477f41c540ceb6a
fintank <finapptank@gmail.com>
2020-11-18 10:13:17 -0500

    added crud functions to api route

--- server.js
+++ server.js
@@ -61,6 +61,41 @@ app.get('/api/candidate/:id', (req, res) => {
   });
 });
 
+//route for all parties
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+
+//route for a single party using id
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
@@ -75,6 +110,20 @@ app.delete('/api/candidate/:id', (req, res) => {
         message: 'successfully deleted',
         changes: this.changes
       });
+    });
+  });
+
+  //delete a party
+  app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({ message: 'successfully deleted', changes: this.changes });
     });
   });
 
