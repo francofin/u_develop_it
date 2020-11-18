b4c4d7c174d64fc4bef9d8266f099283241ae121
fintank <finapptank@gmail.com>
2020-11-18 11:27:44 -0500

    added put request for updates

--- server.js
+++ server.js
@@ -127,6 +127,30 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
   });
 
+  app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `UPDATE candidates SET party_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+  
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: this.changes
+      });
+    });
+  });
 
 // Create a candidate
 app.post('/api/candidate', ({ body }, res) => {
