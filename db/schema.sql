c6a2082efd3f6c218b5392df4b3705c867159112
fintank <finapptank@gmail.com>
2020-11-19 00:04:01 -0500

    modularized code

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS voters;
 
 
 CREATE TABLE parties (
@@ -8,6 +9,13 @@ CREATE TABLE parties (
   description TEXT
 );
 
+CREATE TABLE voters (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
 
 CREATE TABLE candidates (
